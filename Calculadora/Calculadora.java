7cb30959f7a0b488d5d09c1cb57278d02bfaa3f5
Rafael-FV <rafaelvenosa02@gmail.com>
2024-05-15 19:43:49 -0300

    Primeira versão da calculadora

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Object[] itens = { "Soma", "Subtração", "Multiplicação", "Divisão" };
+        Object selectedValue = JOptionPane.showInputDialog(null,
+                "Selecione a operaçao que deseja fazer", "Calculadora",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                itens, itens [0]);
+
+        if (selectedValue == "Soma") {
+            String inputValue1 = JOptionPane.showInputDialog("Digite o primeiro valor");
+            String inputValue2 = JOptionPane.showInputDialog("Agora digite o segundo valor a ser somado");
+            double resultado = Double.valueOf(inputValue1.replace(",", ".")).doubleValue() + Double.valueOf(inputValue2.replace(",", ".")).doubleValue();
+            JOptionPane.showMessageDialog(null, "Resultado: A soma é igual a " + resultado,
+                    "Calculadora", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if (selectedValue == "Subtração") {
+            String inputValue1 = JOptionPane.showInputDialog("Digite o valor do minuendo");
+            inputValue1.replaceAll(",", ".");
+            String inputValue2 = JOptionPane.showInputDialog("Agora digite o valor do subtraendo");
+            inputValue2.replaceAll(",", ".");
+            double resultado = Double.valueOf(inputValue1.replace(",", ".")).doubleValue() - Double.valueOf(inputValue2.replace(",", ".")).doubleValue();
+            JOptionPane.showMessageDialog(null, "Resultado: A subtração é igual a " + resultado,
+                    "Calculadora", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if (selectedValue == "Multiplicação") {
+            String inputValue1 = JOptionPane.showInputDialog("Digite o primeiro valor");
+            inputValue1.replaceAll(",", ".");
+            String inputValue2 = JOptionPane.showInputDialog("Agora digite o segundo valor");
+            inputValue2.replaceAll(",", ".");
+            double resultado = Double.valueOf(inputValue1.replace(",", ".")).doubleValue() * Double.valueOf(inputValue2.replace(",", ".")).doubleValue();
+            JOptionPane.showMessageDialog(null, "Resultado: A multiplicação é igual a " + resultado,
+                    "Calculadora", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if (selectedValue == "Divisão") {
+            String inputValue1 = JOptionPane.showInputDialog("Digite o valor do dividendo");
+            inputValue1.replaceAll(",", ".");
+            String inputValue2 = JOptionPane.showInputDialog("Agora digite o valor do divisor");
+            inputValue2.replaceAll(",", ".");
+            double resultado = Double.valueOf(inputValue1.replace(",", ".")).doubleValue() / Double.valueOf(inputValue2.replace(",", ".")).doubleValue();
+            JOptionPane.showMessageDialog(null, "Resultado: A divisão é igual a " + resultado,
+                    "Calculadora", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
